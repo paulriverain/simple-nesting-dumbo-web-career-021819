bc5f9bd523cb104bfc85bd9c605bcced6d281afc
github username <github email address>
2019-02-01 15:13:34 -0500

    Done.

--- nested.rb
+++ nested.rb
@@ -19,7 +19,6 @@ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 
 	programmer_hash =
  		{
@@ -60,12 +59,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash =
  		{
         :grace_hopper => {
